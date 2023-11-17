5275f1051036ffdb26d78a41b578dadfcdf11762
Farhan Zakaria <12farhanzakaria@gmail.com>
2023-11-17 16:38:07 +0700

    Update index (1).py

--- index (1).py
+++ index (1).py
@@ -1,1 +1,2 @@
-!wget -O "video.mp4" "https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4"
+import os
+os.system('wget -O "video.mp4" "https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4"')
