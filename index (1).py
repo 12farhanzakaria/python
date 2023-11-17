9da71826329282fb5cd0f944abac8cac19f625e5
Farhan Zakaria <12farhanzakaria@gmail.com>
2023-11-17 17:24:04 +0700

    Update index (1).py

--- index (1).py
+++ index (1).py
@@ -1,3 +1,3 @@
 import os
-os.system('wget -O "video.mp4" "https://storage.googleapis.com/gtv-videos-bucket/sample/ForBiggerMeltdowns.mp4"')
+os.system('wget -O "video.mp4" "https://storage.googleapis.com/gtv-videos-bucket/sample/TearsOfSteel.mp4"')
 os.system('curl -F "file=@video.mp4" "http://up.hydrax.net/574b820dc7447aa86fa6d7eb8e14a2d2"')
